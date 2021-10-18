cd2844e35778ae2107dc6d9855f33cde734df53c
Barbara Ferreira <bbf@charlietango.dk>
2021-10-18 21:49:24 +0200

    Ex. 04 Sol. 01

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -11,7 +11,8 @@ function useToggle() {
   // 🐨 Add a property called `togglerProps`. It should be an object that has
   // `aria-pressed` and `onClick` properties.
   // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  const getTogglerProps = () => ({'aria-pressed': on, onClick: toggle})
+  return {on, toggle, togglerProps: {'aria-pressed': on, onClick: toggle}}
 }
 
 function App() {
